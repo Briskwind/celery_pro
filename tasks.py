85a3a33bbfe15fc235d1996785ea2292d617cfa4
briskwind <943138563@qq.com>
2019-02-25 13:58:25 +0800

    Update

--- tasks.py
+++ tasks.py
@@ -32,38 +32,38 @@ app.conf.update(
         #     "task": "celery_worker.get_xsl_nginx",
         #     "schedule": timedelta(minutes=60),
         # },
-        "get_xsl_nginx_yesterday": {
-            "task": "celery_worker.get_xsl_nginx_yesterday",
-            "schedule": crontab(hour=1, minute=1),
-        },
-
-        # 网签日志相关
-        "get_wq_celery_log": {
-            "task": "celery_worker.get_wq_celery_log",
-            "schedule": timedelta(minutes=30),
-        },
-
-        "get_wq_druglistrpc": {
-            "task": "celery_worker.get_druglistrpc_out",
-            "schedule": timedelta(minutes=20),
-        },
-
-        "get_wq_access_log": {
-            "task": "celery_worker.get_access_log",
-            "schedule": timedelta(minutes=10),
-        },
-
-        "nginx_access": {
-            "task": "celery_worker.get_wangqian_access",
-            "schedule": timedelta(minutes=25),
-
-        },
-
-        "nginx_two_days_ago": {
-            "task": "celery_worker.nginx_log_2_day_ago",
-            "schedule": crontab(hour=1, minute=0),
-
-        },
+        # "get_xsl_nginx_yesterday": {
+        #     "task": "celery_worker.get_xsl_nginx_yesterday",
+        #     "schedule": crontab(hour=1, minute=1),
+        # },
+        #
+        # # 网签日志相关
+        # "get_wq_celery_log": {
+        #     "task": "celery_worker.get_wq_celery_log",
+        #     "schedule": timedelta(minutes=30),
+        # },
+        #
+        # "get_wq_druglistrpc": {
+        #     "task": "celery_worker.get_druglistrpc_out",
+        #     "schedule": timedelta(minutes=20),
+        # },
+        #
+        # "get_wq_access_log": {
+        #     "task": "celery_worker.get_access_log",
+        #     "schedule": timedelta(minutes=10),
+        # },
+        #
+        # "nginx_access": {
+        #     "task": "celery_worker.get_wangqian_access",
+        #     "schedule": timedelta(minutes=25),
+        #
+        # },
+        #
+        # "nginx_two_days_ago": {
+        #     "task": "celery_worker.nginx_log_2_day_ago",
+        #     "schedule": crontab(hour=1, minute=0),
+        #
+        # },
 
         # 数据库备份拉取
         # wq
